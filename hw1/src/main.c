fdc5354f8c4307eca6aeb60fd4357e81e435dd4d
shkennedy <shkennedy@cs.stonybrook.edu>
2016-09-11 17:24:29 -0700

    Started main operations

--- hw1/src/main.c
+++ hw1/src/main.c
@@ -21,18 +21,23 @@ int cat(FILE *f, void *res, char *filename) {
 }
 
 int main(int argc, char **argv) {
-    int runChoice;
-    runChoice = validateargs(argc, argv);
+    int runChoice = validateargs(argc, argv);
     switch (runChoice) {
         case -1:
             return EXIT_FAILURE;
         case 0:
             return EXIT_SUCCESS;
     }
-    printf("%d", nfiles(argv[argc - 1]));
-   // int placeHolder[200];
-   /// size_t placeHolderSize = sizeof(int);
-   /// map("./Documents/CSE320/shkennedy/hw1/", placeHolder, placeHolderSize, cat);
+    int numFiles = nfiles(argv[argc - 1]);
+    void *results, *act;
+    size_t size;
+    switch (runChoice) {
+        case 1: // Analysis
+            results = analysis_space;
+            act = analysis_reduce;
+            size = sizeof(Struct Analysis)    
+    }
+    map(argv[argc - 1], placeHolder, placeHolderSize, cat);
    // reduce();
    // print();
     return EXIT_SUCCESS;
