8459746b2393aa932a80e9e872f69346b1d100e7
Shane Kennedy <shane.kennedy@stonybrook.edu>
2016-09-23 22:05:21 -0400

    Done hw2

--- hw2/src/utfconverter.c
+++ hw2/src/utfconverter.c
@@ -47,7 +47,7 @@ int main(int argc, char** argv) { /**  */
 			print_help(EXIT_FAILURE);
 		}
 	}
-	
+
 	srcStats = malloc(sizeof(struct stat));
 	convStats = malloc(sizeof(struct stat));
 	if (srcFilename != NULL) {
