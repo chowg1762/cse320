3edbc36571b1e77e9c855c6d2751f30f7c721045
Shane Kennedy <shane.kennedy@stonybrook.edu>
2016-10-10 11:03:32 -0400

    installed criterion

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -3,13 +3,13 @@
 #include <errno.h>
 #include "sfmm.h"
 
-#define DOUBLE_WORD 8
+#define DWORD 8
 #define MAX_DATA_SIZE 16
 #define PAGE_SIZE 4096
 
 #define HDRP(ptr) ((sf_free_header*)ptr)
 #define FTRP(ptr) ((sf_footer*)ptr)
-#define NEXT_BLKP(ptr) (ptr + ((sf_free_header*)ptr)->block_size)
+#define NEXT_BLKP(ptr) (ptr + ((sf_free_header*)ptr)->header.block_size)
 #define PREV_BLKP(ptr) (ptr - (((sf_footer*)(ptr - 8))->block_size) + 8)
 #define RMV_NODE(header) (header->prev->next = header->next, (header->next->prev = header->prev))
 
@@ -24,28 +24,16 @@ void* end_of_heap;
 
 sf_free_header* freelist_head = NULL;
 
-/*
-struct __attribute__((__packed__)) sf_header {
-    uint64_t alloc : ALLOC_SIZE_BITS;
-    uint64_t block_size : BLOCK_SIZE_BITS;
-    uint64_t unused_bits : UNUSED_SIZE_BITS;
-    uint64_t padding_size : PADDING_SIZE_BITS;
-}; */
-
 void *coalesce(void *ptr) {
-
+  return NULL;
 }
 
 void *find_fit(size_t size) {
-   
-}
-
-void place(void *ptr, size_t size) {
-
+   return NULL;
 }
 
 void *sf_malloc(size_t size){
-  // Handle trivial requests
+  // Handle trivial cases
   if (size == 0) {
     return NULL;
   }
@@ -53,25 +41,23 @@ void *sf_malloc(size_t size){
   // Search free list for suitable block
   size_t padding = size % MAX_DATA_SIZE; // Handle quad word allignment
   size += padding + MAX_DATA_SIZE; // Add size of padding, header, and footer
-  sf_free_header* cursor = freelist_head, *new_free_header;
-  sf_footer* footer, *new_free_footer;
-  sf_free_header* cursor = freelist_head, *new_free_header;
-  sf_footer* footer, *new_free_footer;
+  sf_free_header *new_free_header, *cursor = freelist_head;
+  sf_footer* footer;
   while (cursor != NULL) {
     if (cursor->header.block_size << 4 >= size) {
       size_t size_difference = cursor->header.block_size - size;
       // Remaining space is large enough for another block
       if (size_difference >= 32) {
         // Make new free block with header and footer
-        new_free_header = cursor + size + padding;
+        new_free_header = ((void*)cursor) + size;
         cursor->prev->next = new_free_header;
         cursor->next->prev = new_free_header;
         new_free_header->header.alloc = 0;
         new_free_header->header.block_size = size_difference >> 4;
         new_free_header->header.padding_size = 0;
-        new_free_footer = (void*)new_free_header + size_difference;
-        new_free_footer->alloc = 0;
-        new_free_footer->block_size = size_difference >> 4;
+        footer = (void*)new_free_header + size_difference;
+        footer->alloc = 0;
+        footer->block_size = size_difference >> 4;
         // Replace cursor with new free block in free list
         new_free_header->prev = cursor->prev;
         new_free_header->next = cursor->next;
@@ -84,7 +70,7 @@ void *sf_malloc(size_t size){
       footer = (void*)cursor + cursor->header.block_size - 8;
       footer->alloc = 1;
       footer->block_size = cursor->header.block_size;
-      return cursor + DOUBLE_WORD; // Payload
+      return cursor + DWORD; // Payload
     } else {
       cursor = cursor->next;
     }
@@ -97,9 +83,9 @@ void *sf_malloc(size_t size){
   }
   void* brk_pos = sf_sbrk(0);
   
-  footer = brk_pos - DOUBLE_WORD;
+  footer = brk_pos - DWORD;
   if (footer->alloc == 0) {
-    cursor = ((void*)footer) - footer->block_size + DOUBLE_WORD;
+    cursor = ((void*)footer) - footer->block_size + DWORD;
   } else {
     cursor = (sf_free_header*)brk_pos;
   }
@@ -108,13 +94,13 @@ void *sf_malloc(size_t size){
   cursor->header.block_size = size + padding;
   cursor->header.padding_size = padding;
   // Set Footer
-  footer = ((void*)cursor) + (cursor->header.block_size << 4) - DOUBLE_WORD;
+  footer = ((void*)cursor) + (cursor->header.block_size << 4) - DWORD;
   footer->alloc = 1;
   footer->block_size = cursor->header.block_size;
 
   // Make free block for new heap area
-  new_free_header = ((void*)footer) + DOUBLE_WORD;
-  new_free_footer = brk_pos + PAGE_SIZE - DOUBLE_WORD;
+  new_free_header = ((void*)footer) + DWORD;
+  new_free_footer = brk_pos + PAGE_SIZE - DWORD;
   new_free_header->header.alloc = 0;
   new_free_header->header.block_size = ((void*)new_free_footer) - ((void*)new_free_header);
   new_free_footer->alloc = 0;
@@ -126,16 +112,19 @@ void *sf_malloc(size_t size){
   freelist_head = new_free_header;
 
   // Return start of payload
-  return cursor + DOUBLE_WORD;
+  return cursor + DWORD;
 }
 
-void sf_free(void *ptr){
+void sf_free(void *ptr) {
+  // Handle trivial cases
+  if (ptr == NULL) {
+    return;
+  }
+
   // Check if neighboring blocks are free
-  void *cursor;
-  size_t size, prev_alloc = 0, next_alloc = 0;
+  size_t size = HDRP(ptr - DWORD)->header.block_size, prev_alloc = 0, next_alloc = 0;
   sf_free_header *header;
   sf_footer *footer;
-  size = HDRP(ptr)->header.block_size;
 
   if (ptr > start_of_heap) {
     prev_alloc = HDRP(PREV_BLKP(ptr))->header.alloc;
@@ -156,7 +145,7 @@ void sf_free(void *ptr){
     footer = FTRP((void*)next_header);
     header->header.alloc = 0;
     header->header.block_size += next_header->header.block_size;
-    footer = header->header.block_size;
+    footer->block_size = header->header.block_size;
     RMV_NODE(next_header);
   } 
 
